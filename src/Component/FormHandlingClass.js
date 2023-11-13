837a2b1e3fbfa5dc3202ee221e7444b785856c6e
Rajesh Choudhary <101490518+coding-At-GitHub@users.noreply.github.com>
2023-11-13 09:28:18 +0530

    Update FormHandlingClass.js

--- src/Component/FormHandlingClass.js
+++ src/Component/FormHandlingClass.js
@@ -1,7 +1,6 @@
 import React, { Component, useState, useEffect } from 'react';
  import React, { Component, useState, useEffect } from 'react';
  import { Component, useState, useEffect } from 'react';
-import   
 export default class FormHandlingClass extends Component {
 
     constructor(props){
